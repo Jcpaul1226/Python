17cb701748a2f1972ca5c6d529de82204a017897
Jcpaul1226 <155691679+Jcpaul1226@users.noreply.github.com>
2024-09-19 18:28:08 -0700

    Add files via upload
    updated code

--- Gmail to Excel/Gmail_to_Excel.py
+++ Gmail to Excel/Gmail_to_Excel.py
@@ -1,29 +1,15 @@
-# https://youtu.be/K21BSZPFIjQ
-"""
-Extract selected mails from your gmail account
-
-1. Make sure you enable IMAP in your gmail settings
-(Log on to your Gmail account and go to Settings, See All Settings, and select
- Forwarding and POP/IMAP tab. In the "IMAP access" section, select Enable IMAP.)
-
-2. If you have 2-factor authentication, gmail requires you to create an application
-specific password that you need to use. 
-Go to your Google account settings and click on 'Security'.
-Scroll down to App Passwords under 2 step verification.
-Select Mail under Select App. and Other under Select Device. (Give a name, e.g., python)
-The system gives you a password that you need to use to authenticate from python.
-
-"""
-
 # Importing libraries
 import imaplib
 import email
-import xlsxwriter
+import os
+import pandas as pd
+import time
 
+start= time.time()
 #pip install pyyaml
 import yaml  #To load saved login credentials from a yaml file
 
-with open("credentials2.yml") as f:
+with open("credentials.yml") as f:
     content = f.read()
     
 # from credentials.yml import user name and password
@@ -45,52 +31,88 @@ my_mail.login(user, password)
 my_mail.select('inbox')
 
 #Define Key and Value for email search
-#For other keys (criteria): https://gist.github.com/martinrusev/6121028#file-imap-search
-key = 'SUBJECT'
-value = 'application'
-_, data = my_mail.search(None, key, value)  #Search for emails with specific key and value
+#For other keys check out imap_keys.txt document
+_, data1 = my_mail.search(None, "BODY","application")  
+print("Found ", len(data1[0].split()), " emails containing application")
 
-mail_id_list = data[0].split()  #IDs of all emails that we want to fetch 
+_, data2 = my_mail.search(None, "BODY","screen")  
+print("Found ", len(data2[0].split()), " emails containing screen")
 
+_, data3 = my_mail.search(None, "SUBJECT","interview")  
+_, data4 = my_mail.search(None, "BODY","interview")  
+print("Found ", len(data3[0].split()) + len(data4[0].split()), " emails containing interview")
+
+datai = data1 + data2 + data3 + data4
+datai = datai[0].split()
+dataf = list(set(datai) )   #remove duplicate emails
+print("Removing duplicate emails and extracting the rest")
+
+mail_id_list = dataf  #IDs of all emails that we want to fetch 
 msgs = [] # empty list to capture all messages
+
 #Iterate through messages and extract data into the msgs list
 for num in mail_id_list:
     typ, data = my_mail.fetch(num, '(RFC822)') #RFC822 returns whole message (BODY fetches just body)
     msgs.append(data)
 
-#create the excel sheet
-index = 0
-workbook = xlsxwriter.Workbook("applications.xlsx")
-worksheet = workbook.add_worksheet("firstSheet")
-worksheet.write(index,0,"Sender")
-worksheet.write(index,1,"subject")
+print("retrieved ", len(mail_id_list), " emails")
 
+#words to remove from sender
+remove = ["com","online", "apply","workday","career","talent","data","jobs","apply","alert","Indeed","Linkedin"
+          ,"my","linkedin","indeed","no","reply","talent","recruiting","message","wayup","applicant","talent","aqcuisition",
+          "Workday","mail"]
+
+#check if excel sheet already exists
+path = 'C:\\Users\\jcpau\\OneDrive\\Desktop\\coding stuff\\Email\\applications1.xlsx'
+exists = os.path.isfile(path)
+
+#create the excel sheet
+if(exists == True):
+       print("excel found, creating new sheet")
+       os.remove("applications1.xlsx")
+else:
+    print("Creating excel sheet")
+
+sender= []
+application = []
+subject1 = []
+
+for msg in msgs[::-1]: 
+    step = 'application'
+    body = "none"
+    subject = "none"
+    for response_part in msg:
+        if type(response_part) is tuple:
+            my_msg=email.message_from_bytes((response_part[1]))
+            #Remove special characters from subject and sender
+            subject = ''.join(letter for letter in my_msg["subject"] if letter.isalnum())
+            f = ''.join(letter for letter in my_msg['from'] if letter.isalnum())
+            for i in remove:
+                f = f.replace(i,'')
+            #clean up the body portion of email
+            for part in my_msg.walk():  
+                #print(part.get_content_type())
+                if part.get_content_type() == 'text/plain':
+                    body = part.get_payload()
+                    #categorize what step of application each email is
+                    if "interview" in subject or "Interview" in subject:
+                        step = "interview"   
+                    elif "screen" in body or "screen" in subject:
+                        step =  "screen"
+    sender.append(f)
+    application.append(step)
+    subject1.append(subject)
+                
+print("Applications: ", application.count("application"))
+print("screenings: ", application.count("interview") + application.count("screen"))
+print("interviews: ", application.count("interview"))
+d = {'sender': sender, 'step': application, 'subject': subject1}
+df = pd.DataFrame(data = d)
+print("--- %s seconds ---" % (time.time() - start))
+df.to_excel("applications1.xlsx")
 
 #In a multipart e-mail, email.message.Message.get_payload() returns a 
 # list with one item for each part. The easiest way is to walk the message 
 # and get the payload on each part:
 # https://stackoverflow.com/questions/1463074/how-can-i-get-an-email-messages-text-content-using-python
 
-# NOTE that a Message object consists of headers and payloads.
-for msg in msgs[::-1]:
-    for response_part in msg:
-        if type(response_part) is tuple:
-            my_msg=email.message_from_bytes((response_part[1]))
-            worksheet.write(index+1,1, my_msg['subject'])
-            worksheet.write(index+1,0, my_msg['from'])
-            index += 1
-
-workbook.close()
-"""for msg in msgs[::-1]:
-    for response_part in msg:
-        if type(response_part) is tuple:
-            my_msg=email.message_from_bytes((response_part[1]))
-            print("_________________________________________")
-            print ("subj:", my_msg['subject'])
-            print ("from:", my_msg['from'])
-            print ("body:")
-            for part in my_msg.walk():  
-                #print(part.get_content_type())
-                if part.get_content_type() == 'text/plain':
-                    print (part.get_payload())
-"""
